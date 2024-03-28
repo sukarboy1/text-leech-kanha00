4e28bbd6378cc1a062e0ef70ce74ec657cd94f05
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-03-28 18:58:29 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -4,4 +4,4 @@
 
 api_id = "20945078"
 api_hash = "93f6b8ce4bb0ab61b4c7e42187f2aa64"
-bot_token = "6883697340:AAHzIXSXLniDX_OOO-tSV--S99K5zgep0MM"
+bot_token = "6298609764:AAHZWQcHTq5DlmRH1TvBCRE9_gxW_5UqXnk"
