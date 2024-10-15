313cff092c6525fe9a366312f8574621303ad917
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-10-15 19:13:31 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -2,6 +2,6 @@
 #https://t.me/Sonickuwalupdate
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 
-api_id = "20945078"
-api_hash = "93f6b8ce4bb0ab61b4c7e42187f2aa64"
-bot_token = "7771113355:AAGl_sLxKm_FXsKdlOw7SH96XhyuELYf0d4"
+api_id = ""
+api_hash = ""
+bot_token = ""
